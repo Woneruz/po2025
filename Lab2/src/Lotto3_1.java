ffd2486b4a5cc625b437269ef8ad1aad86967df2
Arkadiusz Baran <qWoneruzq@gmail.com>
2025-10-14 18:07:26 +0200

    Zadanie 3 - rozwiązanie

--- Lab2/src/Lotto3_1.java
+++ Lab2/src/Lotto3_1.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Lotto3 {
+public class Lotto3_1 {  // TO JEST WERSJA Z WYPISYWANIEM CO 100000 LOSOWAŃ (żeby było widać jak działa, ale żeby nie było zbyt dużego spamu w konsoli)
     public static void main(String[] args) {
 
         Random r = new Random();
+
 
         ArrayList<Integer> typy = new ArrayList<>();
         while (typy.size() < 6) {
@@ -34,6 +35,10 @@ public class Lotto3 {
 
             liczbaLosowan++;
 
+            if (liczbaLosowan % 100000 == 0) {
+                System.out.println("Losowanie #" + liczbaLosowan + ": " + los);
+            }
+
             if (los.containsAll(typy) && typy.containsAll(los)) {
                 break;
             }
@@ -42,7 +47,7 @@ public class Lotto3 {
         long koniec = System.currentTimeMillis();
         long czas = koniec - start;
 
-
+        System.out.println("TO JEST WERSJA Z WYPISYWANIEM CO 100000 LOSOWAŃ");
         System.out.println("Wylosowano pełne trafienie po: " + liczbaLosowan + " losowaniach");
         System.out.println("Czas działania programu to: " + czas + " ms");
     }
